33d02e0705285d4051cb43d336eb72e67e51c43c
joseph_scoggins <borninthecaul@gmail.com>
2016-08-22 23:26:09 -0700

    adds links

---
+++ nav.php
@@ -0,0 +1,23 @@
+<div class="flexInnerNav">
+  <div class="flexOuter">
+    <div class="button"><a href="#">about us</a></div>
+  </div>
+  <div class="flexOuter">
+    <div class="button"><a href="#">birds</a></div>
+  </div>
+  <div class="flexOuter">
+    <div class="button">
+      <a href="https://github.com/geoSkogen/php_sql_tablejoins">locations</a>
+    </div>
+  </div>
+  <div class="flexOuter">
+    <div class="button"><a href="#">join 2 tables</a></div>
+  </div>
+  <div class="flexOuter">
+    <div class="button"><a href="#">join 3 tables</a></div>
+  </div>
+  <div class="flexOuter">
+    <div class="button">
+      <a href="http://geoskogen.github.io/joseph_scoggins/">home</a></div>
+  </div>
+</div>
